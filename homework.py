459e608f092e8dba6bca6b38e4e6ac50b84be4dd
Marat <marat63.84@yandex.ru>
2022-06-13 16:37:47 +0300

    Исправил замечания ревью третий раз

--- homework.py
+++ homework.py
@@ -94,15 +94,11 @@ def check_response(response):
         if isinstance(response) == dict:
             response['current_date']
             homeworks = response['homeworks']
-        if isinstance(homeworks) == list:
             return homeworks
-        if not isinstance(response, list):
-            raise TypeError(
-                f'Ответ от API не является списком: response = {response}'
-            )
-
     except Exception:
-        raise TypeError('Ответ от Домашки не словарь')
+        raise TypeError(
+                f'Ответ от API не является словарем: response = {response}'
+            )
 
 
 def parse_status(homework):
